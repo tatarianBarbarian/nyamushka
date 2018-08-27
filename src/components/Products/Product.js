593a625eedcc0a27405b9a7a566c6a3182b7605e
Felix Khafizov (T) <asssama212@gmail.com>
2018-08-28 01:16:30 +0300

    Make ux understandable

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -19,6 +19,7 @@ class Product extends Component {
 
     handleBuyClick = () => {
         this.setState(Object.assign(this.state, {
+            clicked: !this.state.clicked,
             selected: !this.state.selected
         }));
     }
@@ -30,30 +31,71 @@ class Product extends Component {
     }
 
     render() {
-        const {taste, portions, bonus, mass, disabled} = this.props.data;
-        const {selected} = this.state;
+        const {taste, 
+               portions, 
+               bonus, 
+               mass, 
+               disabled,
+               description} = this.props.data;
         
-        return(
-            <div className={`product 
-                                     ${disabled === true 
-                                        ? "product--disabled" 
-                                        : ''}`}>
-                <div className={`product__main ${selected === true ? 'product__main--selected' : ''}`} 
-                     onClick={this.handleProductClick}
-                     onMouseLeave={this.handleMouseLeave}>
-                    <span className="product__slogan"> Сказочное заморское яство </span>
-                    <h2 className="product__title">Нямушка</h2>
-                    <h3 className="product__taste">{taste}</h3>
-                    <p className="u-c--grey u-mt--20"><span className="u-fw--bold">{portions}</span> порций</p><br />
-                    <span className="product__mass">{mass} <br /><span>кг</span></span>  
+        const {selected, 
+               clicked} = this.state;
+        
+        if (selected === true) {
+            return(
+                <div className={`product 
+                                         ${disabled === true 
+                                            ? "product--disabled" 
+                                            : ''}`}>
+                    <div className={`product__main product__main--selected`} 
+                         onClick={this.handleProductClick}
+                         onMouseLeave={this.handleMouseLeave}>
+                        <span className="product__slogan"> Сказочное заморское яство </span>
+                        <h2 className="product__title">Нямушка</h2>
+                        <h3 className="product__taste">{taste}</h3>
+                        <p className="u-c--grey u-mt--20"><span className="u-fw--bold">{portions}</span> порций</p><br />
+                        <span className="product__mass">{mass} <br /><span>кг</span></span>  
+                    </div>
+                    <p className="u-f--13 u-c--white u-ff--exo u-ta--center">{description}</p>
                 </div>
-                {disabled === false  
-                   ? <p className="u-f--13 u-c--white u-ff--exo u-ta--center">Чего сидишь? Порадуй котэ, 
-                        <span className="product__buy" onClick={this.handleBuyClick}>купи</span></p>
-                   : <p className="u-f--13 u-c--yellow u-ff--exo u-ta--center">Печалька, с курой закончился</p>
-                }
-            </div>
-        )
+            )
+        } else if (disabled === true) {
+            return(
+                <div className="product  product--disabled">
+                    <div className={`product__main`} 
+                         onClick={this.handleProductClick}
+                         onMouseLeave={this.handleMouseLeave}>
+                        <span className="product__slogan"> Сказочное заморское яство </span>
+                        <h2 className="product__title">Нямушка</h2>
+                        <h3 className="product__taste">{taste}</h3>
+                        <p className="u-c--grey u-mt--20"><span className="u-fw--bold">{portions}</span> порций</p><br />
+                        <span className="product__mass">{mass} <br /><span>кг</span></span>  
+                    </div>
+                    <p className="u-f--13 u-c--yellow u-ff--exo u-ta--center">Печалька, {taste} закончился</p>
+                </div>
+            )
+        } else {
+            return(
+                <div className="product">
+                    <div className={`product__main ${clicked ? 'product__main--pushed' : ''}`} 
+                         onClick={this.handleProductClick}
+                         onMouseLeave={this.handleMouseLeave}>
+                        <span className="product__slogan"> Сказочное заморское яство </span>
+                        <h2 className="product__title">Нямушка</h2>
+                        <h3 className="product__taste">{taste}</h3>
+                        <p className="u-c--grey u-mt--20"><span className="u-fw--bold">{portions}</span> порций</p><br />
+                        <span className="product__mass">{mass} <br /><span>кг</span></span>  
+                    </div>
+                    {!clicked 
+                       ? <p className="u-f--13 u-c--white u-ff--exo u-ta--center">Чего сидишь? Порадуй котэ, &nbsp;
+                            <span className="product__buy" onClick={this.handleBuyClick}>купи</span></p>
+                       : <p className="u-f--13 u-c--white u-ff--exo u-ta--center">{description}</p>
+                    }
+                </div>
+            )
+        }
+
+        
     }
 }
 
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,15 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import Product from './Product';
 
+//TODO: Center entire block
+
 const ProductsBlock = styled.div`
     width: 87.5vw;
+    width: 1120px;
     margin: 0 auto;
+    margin: auto;
 `;
 
 const ProductsCont = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+    width: 100%;
 `;
 
 const ProductsHeading = styled.h1`
@@ -18,13 +23,35 @@ const ProductsHeading = styled.h1`
   font-size: 36px;
   color: white;
   font-weight: 100;
-  text-align: center;  
+  text-align: center;
+  margin-bottom: 20px;  
 `;
 
 const data = [
-    {taste: 'c фуа-гра', portions: 10, bonus: '', mass: '0,5',  description: 'Печень утки разварная с артишоками.', disabled: false},
-    {taste: 'c рыбой', portions: 40, bonus: '', mass: '2',  description: 'Головы щучьи с чесноком да свежайшая сёмгушка', disabled: false},
-    {taste: 'c курой', portions: 100, bonus: '', mass: '5', description: 'Филе из цыплят с трюфелями в бульоне', disabled: true}
+    {
+        taste: 'c фуа-гра',
+        portions: 10,
+        bonus: '',
+        mass: '0,5',
+        description: 'Печень утки разварная с артишоками.', 
+        disabled: false
+    },
+    {
+        taste: 'c рыбой',
+        portions: 40, 
+        bonus: '', 
+        mass: '2',  
+        description: 'Головы щучьи с чесноком да свежайшая сёмгушка', 
+        disabled: false
+    },
+    {
+        taste: 'c курой',
+        portions: 100, 
+        bonus: '', 
+        mass: '5', 
+        description: 'Филе из цыплят с трюфелями в бульоне', 
+        disabled: true
+    }
 ]
 
 let productsArr = data.map((product,key) => {
