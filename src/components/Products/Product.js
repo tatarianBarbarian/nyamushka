1494d0d1922b6c7a47040194ce98647db41f1b38
Felix Khafizov (T) <asssama212@gmail.com>
2018-08-27 23:45:11 +0300

    Add states to products

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -6,22 +6,52 @@ class Product extends Component {
         super(props);
 
         this.state = {
-            disabled: false,
-            selected: false
+            selected: false,
+            clicked: false
         }
     }
+
+    handleProductClick = () => {
+        this.setState(Object.assign(this.state,{
+            clicked: !this.state.clicked
+        }));
+    }
+
+    handleBuyClick = () => {
+        this.setState(Object.assign(this.state, {
+            selected: !this.state.selected
+        }));
+    }
+
+    handleMouseLeave = () => {
+        this.setState(Object.assign(this.state, {
+            selected: this.state.clicked
+        }));
+    }
+
     render() {
-        const {taste, portions, bonus, mass} = this.props;
+        const {taste, portions, bonus, mass, disabled} = this.props.data;
+        const {selected} = this.state;
+        
         return(
-            <div className="product">
-                <div className="product__main">
+            <div className={`product 
+                                     ${disabled === true 
+                                        ? "product--disabled" 
+                                        : ''}`}>
+                <div className={`product__main ${selected === true ? 'product__main--selected' : ''}`} 
+                     onClick={this.handleProductClick}
+                     onMouseLeave={this.handleMouseLeave}>
                     <span className="product__slogan"> Сказочное заморское яство </span>
                     <h2 className="product__title">Нямушка</h2>
                     <h3 className="product__taste">{taste}</h3>
-                    <span><span className="u-fw--bold">{portions}</span> порций</span><br />
+                    <p className="u-c--grey u-mt--20"><span className="u-fw--bold">{portions}</span> порций</p><br />
                     <span className="product__mass">{mass} <br /><span>кг</span></span>  
                 </div>
-                <p>Чего сидишь? Порадуй котэ, <span className="product__buy">купи</span></p>
+                {disabled === false  
+                   ? <p className="u-f--13 u-c--white u-ff--exo u-ta--center">Чего сидишь? Порадуй котэ, 
+                        <span className="product__buy" onClick={this.handleBuyClick}>купи</span></p>
+                   : <p className="u-f--13 u-c--yellow u-ff--exo u-ta--center">Печалька, с курой закончился</p>
+                }
             </div>
         )
     }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,14 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Product from './Product';
 
 const ProductsBlock = styled.div`
-
+    width: 87.5vw;
+    margin: 0 auto;
 `;
 
 const ProductsCont = styled.div`
     display: flex;
-    justify-content: space-evenly;
+    flex-wrap: wrap;
+    justify-content: center;
 `;
 
 const ProductsHeading = styled.h1`
@@ -19,18 +21,29 @@ const ProductsHeading = styled.h1`
   text-align: center;  
 `;
 
+const data = [
+    {taste: 'c фуа-гра', portions: 10, bonus: '', mass: '0,5',  description: 'Печень утки разварная с артишоками.', disabled: false},
+    {taste: 'c рыбой', portions: 40, bonus: '', mass: '2',  description: 'Головы щучьи с чесноком да свежайшая сёмгушка', disabled: false},
+    {taste: 'c курой', portions: 100, bonus: '', mass: '5', description: 'Филе из цыплят с трюфелями в бульоне', disabled: true}
+]
+
+let productsArr = data.map((product,key) => {
+    return <Product key={key} data = {product} />
+})
 
 const Products = () => {
     return(
         <ProductsBlock>
             <ProductsHeading> Ты сегодня покормил кота? </ProductsHeading>
             <ProductsCont>
-                <Product />
-                <Product />
-                <Product />
+                {productsArr}
             </ProductsCont>
         </ProductsBlock>
     )
 }
+
+
+
+
 
 export default Products;
